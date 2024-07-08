6c7ba938bf2c8422b33c5dcff8442f2d2b6c95df
grly <grzegorz.lysyganicz@gmail.com>
2024-07-08 19:52:53 +0200

    Add scaffolding for student service test

---
+++ userManagement/src/test/org/lysygang/application/domain/service/AddStudentServiceTest.java
@@ -0,0 +1,13 @@
+package org.lysygang.application.domain.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddStudentServiceTest {
+
+    @Test
+    void shouldReturnCreatedStudentIdWithSucces() {
+        // todo implement me!
+    }
+}
