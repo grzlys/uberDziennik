ae0abfcd3d7b528c36a4f4b2968ad72eb30d40c5
grly <grzegorz.lysyganicz@gmail.com>
2024-08-01 20:42:22 +0200

    Implement unit test for AddStudentService

--- userManagement/src/test/org/lysygang/application/domain/service/AddStudentServiceTest.java
+++ userManagement/src/test/org/lysygang/application/domain/service/AddStudentServiceTest.java
@@ -1,13 +1,36 @@
 package org.lysygang.application.domain.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.lysygang.adapter.out.persistence.repository.StudentPersistenceAdapter;
+import org.lysygang.application.port.in.AddStudentCommand;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AddStudentServiceTest {
 
+    @InjectMocks
+    private AddStudentService service;
+
+    @Mock
+    private StudentPersistenceAdapter studentPersistenceAdapter;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    void shouldReturnCreatedStudentIdWithSucces() {
-        // todo implement me!
+    void shouldReturnCreatedStudentIdWithSuccess() {
+        // given
+        AddStudentCommand addStudentCommand = new AddStudentCommand("testName", "testLastName");
+        BDDMockito.given(studentPersistenceAdapter.saveStudent(addStudentCommand)).willReturn(20);
+
+        // expect
+        assertThat(service.addNewStudent(addStudentCommand)).isEqualTo(20);
     }
 }
