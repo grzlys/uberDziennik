15af9eb0e4e99ab9cc3a8d5fe59ce0e18dda6760
grly <grzegorz.lysyganicz@gmail.com>
2024-12-12 13:50:38 +0100

    Fix restReader Fix input data
    note: adding listener in importStudentStep is important for restReader, without it, importing job does not work, and throw exception (meanwhile rest return 200...). It was unnecessary for "regular" reader, which take input data form file located in resources.

--- userManagement/src/main/java/org/lysygang/config/BatchProcessingConfiguration.java
+++ userManagement/src/main/java/org/lysygang/config/BatchProcessingConfiguration.java
@@ -9,40 +9,36 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchProcessingConfiguration {
 
-    @Bean
-    public FlatFileItemReader<AddStudentCommand> reader() {
-        return new FlatFileItemReaderBuilder<AddStudentCommand>()
-                .name("studentItemReader")
-                .resource(new ClassPathResource("input.csv"))
-                .delimited()
-                .names("firstName", "lastName")
-                .targetType(AddStudentCommand.class)
-                .build();
-    }
 
     @Bean
     public FlatFileItemReader<AddStudentCommand> restReader(){
         FlatFileItemReader<AddStudentCommand> reader = new FlatFileItemReader<>();
-        reader.setLinesToSkip(1); // pomiń nagłówki
+        skipHeaders(reader);
 
         DefaultLineMapper<AddStudentCommand> lineMapper = new DefaultLineMapper<>();
         lineMapper.setFieldSetMapper(fieldSet ->
                 new AddStudentCommand(fieldSet.readString("firstName"), fieldSet.readString("lastName")));
-        lineMapper.setLineTokenizer(new DelimitedLineTokenizer());
+
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("firstName", "lastName");
+
+        lineMapper.setLineTokenizer(tokenizer);
         reader.setLineMapper(lineMapper);
 
         return reader;
+    }
+
+    private static void skipHeaders(FlatFileItemReader<AddStudentCommand> reader) {
+        reader.setLinesToSkip(1);
     }
 
     @Bean
@@ -61,14 +57,13 @@ public class BatchProcessingConfiguration {
 
     @Bean
     public Step importStudentStep(JobRepository jobRepository,
-//                                  ItemReader<AddStudentCommand> reader,
-                                  FlatFileItemReader<AddStudentCommand>reader,
+                                  FlatFileItemReader<AddStudentCommand>restReader,
                                   ItemWriter<AddStudentCommand> writer,
                                   PlatformTransactionManager transactionManager) {
         return new StepBuilder("importStudentStep", jobRepository)
                 .<AddStudentCommand, AddStudentCommand>chunk(3, transactionManager)
-//                .listener(new FileReaderListener(reader))
-                .reader(reader)
+                .listener(new FileReaderListener(restReader))
+                .reader(restReader)
                 .writer(writer)
                 .allowStartIfComplete(true) // maybe wiping out batch job tables can make to delete this line of code
                 .build();
